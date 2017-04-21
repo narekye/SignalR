d6134717c9e9bdaa71ebcc0aeeca121bcb2d15a6
narekye <yegoryan.narek@gmail.com>
2017-04-21 17:15:36 -0600

    :ok: Hub update :ok:

--- Chat_With_SignalR/Simple_Chat/Hubs/ChatHub.cs
+++ Chat_With_SignalR/Simple_Chat/Hubs/ChatHub.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNet.SignalR;
-using Simple_Chat.Models;
-
-namespace Simple_Chat.Hubs
+﻿namespace Simple_Chat.Hubs
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.SignalR;
+    using Models;
+
     public class ChatHub : Hub
     {
         private static List<User> Users = new List<User>();
@@ -13,10 +15,31 @@ namespace Simple_Chat.Hubs
             Clients.All.addMessage(name, message);
         }
 
-        public void Connect()
+        public void Connect(string username)
         {
-            
+            var id = Context.ConnectionId;
+
+            if (Users.All(x => x.ConnectionId != id))
+            {
+                Users.Add(new User() { ConnectionId = id, Name = username });
+                Clients.Caller.onConnected(id, username, Users);
+
+                Clients.AllExcept(id).onNewUserConnected(id, username);
+            }
         }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            User item = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (!ReferenceEquals(item, null))
+            {
+                Users.Remove(item);
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.Name);
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+
         public void Hello()
         {
             Clients.All.hello();
