c026c3a295656cbfa6c1c68ae115b43ac8dfcf62
narekye <yegoryan.narek@gmail.com>
2017-04-21 17:39:56 -0600

    :ok: some checks :ok:

--- Chat_With_SignalR/Simple_Chat/Scripts/util.js
+++ Chat_With_SignalR/Simple_Chat/Scripts/util.js
@@ -2,54 +2,53 @@
 
     $('#chatBody').hide();
     $('#loginBlock').show();
-    // Ссылка на автоматически-сгенерированный прокси хаба
     var chat = $.connection.chatHub;
-    // Объявление функции, которая хаб вызывает при получении сообщений
+    
     chat.client.addMessage = function (name, message) {
-        // Добавление сообщений на веб-страницу 
+    
         $('#chatroom').append('<p><b>' + htmlEncode(name)
             + '</b>: ' + htmlEncode(message) + '</p>');
     };
 
-    // Функция, вызываемая при подключении нового пользователя
+    
     chat.client.onConnected = function (id, userName, allUsers) {
 
         $('#loginBlock').hide();
         $('#chatBody').show();
-        // установка в скрытых полях имени и id текущего пользователя
+    
         $('#hdId').val(id);
         $('#username').val(userName);
         $('#header').html('<h3>Welcome, ' + userName + '</h3>');
 
-        // Добавление всех пользователей
+    
         for (var i = 0; i < allUsers.length; i++) {
 
             AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
         }
     }
 
-    // Добавляем нового пользователя
+    
     chat.client.onNewUserConnected = function (id, name) {
 
         AddUser(id, name);
     }
 
-    // Удаляем пользователя
+    
     chat.client.onUserDisconnected = function (id, userName) {
 
         $('#' + id).remove();
     }
 
-    // Открываем соединение
+    
     $.connection.hub.start().done(function () {
 
         $('#sendmessage').click(function () {
-            // Вызываем у хаба метод Send
+    
             chat.server.send($('#username').val(), $('#message').val());
             $('#message').val('');
         });
 
-        // обработка логина
+    
         $("#btnLogin").click(function () {
 
             var name = $("#txtUserName").val();
@@ -62,12 +61,12 @@
         });
     });
 });
-// Кодирование тегов
+
 function htmlEncode(value) {
     var encodedValue = $('<div />').text(value).html();
     return encodedValue;
 }
-//Добавление нового пользователя
+
 function AddUser(id, name) {
 
     var userId = $('#hdId').val();
