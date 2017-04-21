774125e1f6acb9e9be5c584ac54c40dee3926067
narekye <yegoryan.narek@gmail.com>
2017-04-21 17:03:01 -0600

    :ok: SignalR added, user model added :ok:

---
+++ Chat_With_SignalR/Simple_Chat/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Simple_Chat.Models
+{
+    public class User
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Chat_With_SignalR/Simple_Chat/Startup.cs
+++ Chat_With_SignalR/Simple_Chat/Startup.cs
@@ -9,6 +9,7 @@ namespace Simple_Chat
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
