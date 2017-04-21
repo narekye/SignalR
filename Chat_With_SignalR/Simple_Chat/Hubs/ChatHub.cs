4f401cd652239672af5f820e6c0f4f5f4840d93d
narekye <yegoryan.narek@gmail.com>
2017-04-21 17:06:53 -0600

    :ok: Hub added :ok:

---
+++ Chat_With_SignalR/Simple_Chat/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using Simple_Chat.Models;
+
+namespace Simple_Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static List<User> Users = new List<User>();
+
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+
+        public void Connect()
+        {
+            
+        }
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
