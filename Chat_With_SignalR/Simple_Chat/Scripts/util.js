5cde8443314522103ae5752d366ca5f4ea523dfe
narekye <yegoryan.narek@gmail.com>
2017-04-21 17:31:31 -0600

    :ok: added util.js file which implements client side SignalR :ok:

---
+++ Chat_With_SignalR/Simple_Chat/Scripts/util.js
@@ -0,0 +1,79 @@
+﻿$(function () {
+
+    $('#chatBody').hide();
+    $('#loginBlock').show();
+    // Ссылка на автоматически-сгенерированный прокси хаба
+    var chat = $.connection.chatHub;
+    // Объявление функции, которая хаб вызывает при получении сообщений
+    chat.client.addMessage = function (name, message) {
+        // Добавление сообщений на веб-страницу 
+        $('#chatroom').append('<p><b>' + htmlEncode(name)
+            + '</b>: ' + htmlEncode(message) + '</p>');
+    };
+
+    // Функция, вызываемая при подключении нового пользователя
+    chat.client.onConnected = function (id, userName, allUsers) {
+
+        $('#loginBlock').hide();
+        $('#chatBody').show();
+        // установка в скрытых полях имени и id текущего пользователя
+        $('#hdId').val(id);
+        $('#username').val(userName);
+        $('#header').html('<h3>Welcome, ' + userName + '</h3>');
+
+        // Добавление всех пользователей
+        for (var i = 0; i < allUsers.length; i++) {
+
+            AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
+        }
+    }
+
+    // Добавляем нового пользователя
+    chat.client.onNewUserConnected = function (id, name) {
+
+        AddUser(id, name);
+    }
+
+    // Удаляем пользователя
+    chat.client.onUserDisconnected = function (id, userName) {
+
+        $('#' + id).remove();
+    }
+
+    // Открываем соединение
+    $.connection.hub.start().done(function () {
+
+        $('#sendmessage').click(function () {
+            // Вызываем у хаба метод Send
+            chat.server.send($('#username').val(), $('#message').val());
+            $('#message').val('');
+        });
+
+        // обработка логина
+        $("#btnLogin").click(function () {
+
+            var name = $("#txtUserName").val();
+            if (name.length > 0) {
+                chat.server.connect(name);
+            }
+            else {
+                alert("Enter name..!!");
+            }
+        });
+    });
+});
+// Кодирование тегов
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
+//Добавление нового пользователя
+function AddUser(id, name) {
+
+    var userId = $('#hdId').val();
+
+    if (userId != id) {
+
+        $("#chatusers").append('<p id="' + id + '"><b>' + name + '</b></p>');
+    }
+}
