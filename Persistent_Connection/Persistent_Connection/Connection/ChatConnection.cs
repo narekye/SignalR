ab732c55e3a72afd87a5e8e87e7920ab47209f34
narekye <yegoryan.narek@gmail.com>
2017-05-04 15:54:20 -0600

    :v: added OWIN startup class :v:

---
+++ Persistent_Connection/Persistent_Connection/Connection/ChatConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Persistent_Connection.Models;
+
+namespace Persistent_Connection.Connection
+{
+    public class ChatConnection : PersistentConnection
+    {
+        /// <inheritdoc />
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            Data data = new Data() { Name = "Server message", Message = "Client " + connectionId + " is connected to chat" };
+            return Connection.Broadcast(data);
+        }
+    }
+}
--- Persistent_Connection/Persistent_Connection/Models/Data.cs
+++ Persistent_Connection/Persistent_Connection/Models/Data.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Persistent_Connection.Models
+﻿namespace Persistent_Connection.Models
 {
     public class Data
     {
+        public string Name { get; set; }
+        public string Message { get; set; } 
     }
 }
