ffe467988e73136b3f3699031b5bb0ad5d53ae3e
narekye <yegoryan.narek@gmail.com>
2017-05-04 21:48:52 -0600

    :v: final commit :v:

--- Persistent_Connection/Persistent_Connection/Connection/ChatConnection.cs
+++ Persistent_Connection/Persistent_Connection/Connection/ChatConnection.cs
@@ -7,21 +7,18 @@ namespace Persistent_Connection.Connection
 {
     public class ChatConnection : PersistentConnection
     {
-        /// <inheritdoc />
         protected override Task OnConnected(IRequest request, string connectionId)
         {
             Data data = new Data() { Name = "Server message", Message = "Client " + connectionId + " is connected to chat" };
             return Connection.Broadcast(data);
         }
 
-        /// <inheritdoc />
         protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
             Data chatdata = JsonConvert.DeserializeObject<Data>(data);
             return Connection.Broadcast(chatdata);
         }
 
-        /// <inheritdoc />
         protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
         {
             Data chatdata = new Data() { Name = "Server message ", Message = "Client with " + connectionId + " id disconnected" };
