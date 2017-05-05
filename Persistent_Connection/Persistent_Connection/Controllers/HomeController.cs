a351c4e7888773a1191dfd967d8ea67a588667ee
narekye <yegoryan.narek@gmail.com>
2017-05-04 21:35:03 -0600

    :v: clean up code :v:

--- Persistent_Connection/Persistent_Connection/Controllers/HomeController.cs
+++ Persistent_Connection/Persistent_Connection/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
+﻿using System.Web.Mvc;
 namespace Persistent_Connection.Controllers
 {
     public class HomeController : Controller
@@ -29,7 +24,7 @@ namespace Persistent_Connection.Controllers
 
         public ActionResult Chat()
         {
-            return this.View();
+            return View();
         }
     }
 }
